c8eb1b2b6f48400df27bc5dfd907998ba363742a
Nishthaag30 <nishtha.agrawal_cs.ccv20@gla.ac.in>
2022-07-17 23:11:23 +0530

    js file added

---
+++ jsfiles/iphones.js
@@ -0,0 +1,39 @@
+// iphone 13 max pro
+        var img1= document.getElementById("img1");
+        var btn1= document.getElementById("btn1");
+        var btn2= document.getElementById("btn2");
+        var btn3= document.getElementById("btn3");
+        var btn4= document.getElementById("btn4");
+        var btn5= document.getElementById("btn5");
+        btn1.onmouseover= ()=> {
+            img1.src= '../images/iphone13pro/iphone13promax.png';
+        };
+        btn2.onmouseover= ()=> {
+            img1.src= '../images/iphone13pro/iphone13pro_gold.png';
+        };
+        btn3.onmouseover= ()=> {
+            img1.src= '../images/iphone13pro/iphone13pro_graphite.png';
+        };
+        btn4.onmouseover= ()=> {
+            img1.src= '../images/iphone13pro/iphone13pro_silver.png';
+        };
+        btn5.onmouseover= ()=> {
+            img1.src= '../images/iphone13pro/iphone13pro_blue.png';
+        };
+        btn1.onmouseleave= ()=> {
+            img1.src= '../images/iphone13pro/iphone13promax.png';
+        };
+        btn2.onmouseleave= ()=> {
+            img1.src= '../images/iphone13pro/iphone13promax.png';
+        };
+        btn3.onmouseleave= ()=> {
+            img1.src= '../images/iphone13pro/iphone13promax.png';
+        };
+        btn4.onmouseleave= ()=> {
+            img1.src= '../images/iphone13pro/iphone13promax.png';
+        };
+        btn5.onmouseleave= ()=> {
+            img1.src= '../images/iphone13pro/iphone13promax.png';
+        };
+
+//iphone 13
