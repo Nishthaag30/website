a0ac19022fca61fe8a8d9b9a4ffdecae950e6880
Nishthaag30 <nishtha.agrawal_cs.ccv20@gla.ac.in>
2022-07-18 01:24:25 +0530

    new changes

--- jsfiles/iphones.js
+++ jsfiles/iphones.js
@@ -37,3 +37,46 @@
         };
 
 //iphone 13
+var img2= document.getElementById("img2");
+        var btn1= document.getElementById("bt1");
+        var btn2= document.getElementById("bt2");
+        var btn3= document.getElementById("bt3");
+        var btn4= document.getElementById("bt4");
+        var btn5= document.getElementById("bt5");
+        var btn6= document.getElementById("bt6");
+        btn1.onmouseover= ()=> {
+            img2.src= '../images/iphone13/iphone13_red.png';
+        };
+        btn2.onmouseover= ()=> {
+            img2.src= '../images/iphone13/iphone13_blue.png';
+        };
+        btn3.onmouseover= ()=> {
+            img2.src= '../images/iphone13/iphone13_green.png';
+        };
+        btn4.onmouseover= ()=> {
+            img2.src= '../images/iphone13/iphone13_midnight.png';
+        };
+        btn5.onmouseover= ()=> {
+            img2.src= '../images/iphone13/iphone13_pink.png';
+        };
+        btn6.onmouseover= ()=> {
+            img2.src= '../images/iphone13/iphone13_starlight.png';
+        };
+        btn1.onmouseleave= ()=> {
+            img2.src= '../images/iphone13/iphone13_red.png';
+        };
+        btn2.onmouseleave= ()=> {
+            img2.src= '../images/iphone13/iphone13_red.png';
+        };
+        btn3.onmouseleave= ()=> {
+            img2.src= '../images/iphone13/iphone13_red.png';
+        };
+        btn4.onmouseleave= ()=> {
+            img2.src= '../images/iphone13/iphone13_red.png';
+        };
+        btn5.onmouseleave= ()=> {
+            img2.src= '../images/iphone13/iphone13_red.png';
+        };
+        btn6.onmouseleave= ()=> {
+            img2.src= '../images/iphone13/iphone13_red.png';
+        };
