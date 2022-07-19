d7001e6fb43f7705816d65cbf2ef7954906d3b82
Nishthaag30 <nishtha.agrawal_cs.ccv20@gla.ac.in>
2022-07-19 22:37:26 +0530

    website changed

---
+++ main.js
@@ -0,0 +1,5 @@
+const selectElement = (element) => document.querySelector(element);
+
+selectElement('.mobile-menu').addEvenListener('click',()=> {
+    selectElement('header').classList.toggle('active');
+});
